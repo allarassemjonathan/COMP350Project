258a1a8e7282907115d3bdcc8506d713397f9095
ALLARASSEMJJ20 <ALLARASSEMJJ20@5CG024DG3X>
2023-02-22 14:45:33 -0500

    first commit

---
+++ src/main/java/edu/gcc/comp350/scrumlings/Schedule.java
@@ -0,0 +1,11 @@
+package edu.gcc.comp350.scrumlings;
+
+public class Schedule {
+    public static void main( String [] args){
+
+    }
+
+    public int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/edu/gcc/comp350/scrumlings/ScheduleTest.java
@@ -0,0 +1,16 @@
+package edu.gcc.comp350.scrumlings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScheduleTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void add() {
+    }
+}
