4ef844146acb077a24b04766d92fdfe8ed43b67b
avaHatfield <106991181+avaHatfield@users.noreply.github.com>
2023-03-26 14:14:55 -0400

    add course and conflict

--- src/main/java/edu/gcc/comp350/scrumlings/Driver.java
+++ src/main/java/edu/gcc/comp350/scrumlings/Driver.java
@@ -1,6 +1,7 @@
 package edu.gcc.comp350.scrumlings;
 
 import java.io.FileNotFoundException;
+import java.sql.PseudoColumnUsage;
 import java.util.Scanner;
 
 public class Driver {
@@ -49,14 +50,25 @@ public class Driver {
         this.search = search;
     }
 
-
     // Other Methods
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws Exception {
+        //test
+        String[] timeOne = {"M 12"};
+        String[] timeTwo = {"M 12"};
+        String[] timeThree = {"W 12", "W 13","F 34"};
+        Course testOne = new Course("Comp250", "wolfe", timeOne,"CS", 1, 'A');
+        Course testTwo = new Course("Comp350", "wolfe", timeTwo,"CS", 1, 'A');
+        Schedule s = new Schedule("Ava","");
+        s.getCourses().add(testTwo);
+        s.addCourse(testOne);
+
+
+
         // init
         System.out.println("Welcome to the Scrumlings Semester Scheduler!");
         System.out.println("Enter help for a list of commands");
         // main loop
-        while (true) {
+        while(true) {
             userInput = scnr.nextLine();
             // quitting
             if (userInput.equalsIgnoreCase("quit")) {
